d2a16a4fa9725e313544d220993bb3ec1321f4b7
Ivan Ivanov <slayer_word_11@outlook.com>
2015-08-21 17:47:21 +0300

    add EcmaScript 6 features

---
+++ JavaScript-Next/classes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+}
+
+var person = new Person("Ivan Ivanov", 17);
+
+console.log(person);
---
+++ JavaScript-Next/collections-map.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var map = new Map();
+
+map.set("Ivan Ivanov", 17);
+map.set("Gosho Georgiev", 65);
+map.set("Kiro Zlatniq", 60);
+map.set("Vasil Gochev", 65);
+
+console.log("Ivan Ivanov: ", map.get("Ivan Ivanov"));
+console.log("Gosho Georgiev: ", map.get("Gosho Georgiev"));
+console.log("Kiro Zlatniq: ", map.get("Kiro Zlatniq"));
+console.log("Vasil Gochev: ", map.get("Vasil Gochev"));
